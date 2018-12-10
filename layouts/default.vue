a88f51de66fd9b119bcbfb311c84fde723f26a60
Casper Engelmann <me@casperengelmann.com>
2018-12-10 15:10:58 +0100

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="app">
+    <nav
+      class="navbar"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <nuxt-link
+          to="/"
+          class="navbar-item"
+        >
+          Reins
+        </nuxt-link>
+        
+        <a
+          :class="{ 'is-active' : navOpen }"
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbar"
+          @click="toggleNav()"
+        >
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+      </div>
+
+      <div
+        id="navbar"
+        :class="{ 'is-active' : navOpen }"
+        class="navbar-menu"
+      >
+        <div class="navbar-end">
+          <nuxt-link
+            to="/"
+            class="navbar-item"
+          >
+            Home
+          </nuxt-link>
+          <nuxt-link
+            to="/products"
+            class="navbar-item"
+          >
+            Products
+          </nuxt-link>
+        </div>
+
+      </div>
+    </nav>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  data() {
+    return {
+      navOpen: false
+    }
+  },
+  mounted() {
+    this.getProducts()
+    this.getCart()
+    this.getCartTotals()
+  },
+  methods: {
+    ...mapActions(['getProducts', 'getCart', 'getCartTotals']),
+    toggleNav() {
+      this.navOpen = !this.navOpen
+    }
+  }
+}
+</script>
+
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ store/index.js
@@ -0,0 +1,131 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createHmac from 'create-hmac'
+import OAuth from 'oauth-1.0a'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+const oauth = new OAuth({
+  consumer: {
+    key: process.env.OAUTH_CONSUMER_KEY,
+    secret: process.env.OAUTH_CONSUMER_SECRET
+  },
+  signature_method: 'HMAC-SHA1',
+  hash_function(base_string, key) {
+    return createHmac('sha1', key)
+      .update(base_string)
+      .digest('base64')
+  }
+})
+
+const store = () =>
+  new Vuex.Store({
+    state: {
+      products: [],
+      cart: {},
+      cartTotals: {}
+    },
+    getters: {
+      products: state => state.products,
+      cart: state => state.cart,
+      cartTotals: state => state.cartTotals
+    },
+    mutations: {
+      SET_PRODUCTS(state, payload) {
+        state.products = payload
+      },
+      SET_CART(state, payload) {
+        state.cart = payload
+      },
+      SET_CART_TOTALS(state, payload) {
+        state.cartTotals = payload
+      }
+    },
+    actions: {
+      async getProducts({ commit }) {
+        try {
+          const request = {
+            url: 'https://reins.test/wp-json/wc/v3/products',
+            method: 'GET'
+          }
+
+          const auth = oauth.toHeader(oauth.authorize(request))
+
+          const json = await axios({
+            url: request.url,
+            method: request.method,
+            headers: {
+              ...auth
+            }
+          })
+
+          commit('SET_PRODUCTS', json.data)
+
+          return json.data
+        } catch (err) {
+          // console.error(err);
+        }
+      },
+      async getCart({ commit }) {
+        try {
+          const request = {
+            url: 'https://reins.test/wp-json/wc/v2/cart',
+            method: 'GET'
+          }
+
+          const json = await axios({
+            url: request.url,
+            method: request.method
+          })
+
+          commit('SET_CART', json.data)
+
+          return json.data
+        } catch (err) {
+          // console.error(err);
+        }
+      },
+      async getCartTotals({ commit }) {
+        try {
+          const request = {
+            url: 'https://reins.test/wp-json/wc/v2/cart/totals',
+            method: 'GET'
+          }
+
+          const json = await axios({
+            url: request.url,
+            method: request.method
+          })
+
+          commit('SET_CART_TOTALS', json.data)
+
+          return json.data
+        } catch (err) {
+          // console.error(err);
+        }
+      },
+      async addToCart({ commit }, data) {
+        try {
+          const request = {
+            url: 'https://reins.test/wp-json/wc/v2/cart/add',
+            method: 'POST'
+          }
+
+          const json = await axios({
+            url: request.url,
+            method: request.method,
+            data
+          })
+
+          commit('SET_CART_TOTALS', json.data)
+
+          return json.data
+        } catch (err) {
+          // console.error(err);
+        }
+      }
+    }
+  })
+
+export default store
