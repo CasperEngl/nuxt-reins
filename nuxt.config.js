75e97df710e0ab4a85f0c715fb57f8f888c9fd91
Casper Engelmann <me@casperengelmann.com>
2018-12-26 03:41:22 +0100

    added material icons to head

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,13 +9,27 @@ module.exports = {
   */
   head: {
     title: pkg.name,
-    script: [{ src: 'https://js.stripe.com/v3/' }],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    script: [
+      {
+        src: 'https://js.stripe.com/v3/',
+      },
+    ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
+      },
+    ],
   },
 
   /*
